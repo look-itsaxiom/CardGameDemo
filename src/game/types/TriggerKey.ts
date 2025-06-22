26de4fb591cb3a854c474cb027dbb1102cdb981d
Chase Skibeness <chaseskibbs@gmail.com>
2025-06-22 16:59:29 -0700

    feat: Implement game engine with database integration and board management
    - Added Database class to manage player data, decks, and cards from JSON.
    - Created BoardManager to handle board state and initialization.
    - Developed GamePlayer class for player actions, including deck management and card drawing.
    - Implemented GameStateEngine to manage game phases, player turns, and event triggers.
    - Introduced helper functions for shuffling decks and generating unique card IDs.
    - Defined extensive types for game mechanics, including cards, players, and game state.
    - Established trigger system for game events and actions.

---
+++ src/game/types/TriggerKey.ts
@@ -0,0 +1,30 @@
+export type TriggerKeyBase =
+    | "on_draw_attempt_deck_empty"
+    | "on_card_drawn"
+    | "on_opp_card_drawn"
+    | "on_game_start"
+    | "on_start_turn"
+    | "on_start_draw_phase"
+    | "on_end_draw_phase"
+    | "on_start_level_phase"
+    | "on_end_level_phase"
+    | "on_start_action_phase"
+    | "on_end_action_phase"
+    | "on_start_end_phase"
+    | "on_end_turn"
+    | "on_summon"
+    | "on_summon_damage"
+    | "on_summon_damage_by_this_effect"
+    | "on_summon_destroyed"
+    | "on_summon_destroyed_by_this_effect"
+    | "on_summon_removed_from_game"
+    | "on_effect_cost_paid"
+    | "on_play"
+    | "on_summon_in_occupied_space"
+    | "on_level_up";
+
+export type TriggerKey =
+    | TriggerKeyBase
+    | `${TriggerKeyBase}|${TriggerKeyBase}`
+    | `${TriggerKeyBase}|${TriggerKeyBase}|${TriggerKeyBase}`
+    | `${TriggerKeyBase}|${TriggerKeyBase}|${TriggerKeyBase}|string}`;
